7b505e6231a876b0902fd3d01ce348d560e583d3
Fernando Kanashiro <fernando.kanashiro@vmdesk-02.valemobi.corp>
2019-09-18 07:43:00 -0300

    corrigindo função

--- section-3/Exercises.hs
+++ section-3/Exercises.hs
@@ -8,44 +8,44 @@ module Exercises where
 
     -- converterFarenheit: recebe um valor double e uma temperatura e faz a conversão para Farenheit
 
-    data Temperatura = Celcius | Farenheit | Kelvin
+data Temperatura = Celcius | Farenheit | Kelvin deriving (Show)
 
-    temperaturaBase :: Temperatura -> (Double, Double)
-    temperaturaBase (Celcius) = (0, 100)
-    temperaturaBase (Farenheit) = (32, 212)
-    temperaturaBase (Kelvin) = (273, 373)
+temperaturaBase :: Temperatura -> (Double, Double)
+temperaturaBase (Celcius) = (0, 100)
+temperaturaBase (Farenheit) = (32, 212)
+temperaturaBase (Kelvin) = (273, 373)
 
-    converterCelsius :: Double -> Temperatura -> Double
-    converterCelsius x y = converteEscala x y Celcius
+converterCelsius :: Double -> Temperatura -> (Double, Temperatura)
+converterCelsius x y = converteEscala x y Celcius
 
-    converterKelvin :: Double -> Temperatura -> Double
-    converterKelvin x y = converteEscala x y Kelvin
+converterKelvin :: Double -> Temperatura -> (Double, Temperatura)
+converterKelvin x y = converteEscala x y Kelvin
 
-    converterFarenheit :: Double -> Temperatura -> Double
-    converterFarenheit x y = converteEscala x y Farenheit
+converterFarenheit :: Double -> Temperatura -> (Double, Temperatura)
+converterFarenheit x y = converteEscala x y Farenheit
 
-    converteEscala :: Double -> Temperatura -> Temperatura -> Double
-    converteEscala graus deTemp paraTemp = ((graus - minDeTemp) * (maxParaTemp - minParaTemp) / (maxDeTemp - minDeTemp)) + minParaTemp
-        where minDeTemp = fst(temperaturaBase(deTemp))
-              maxDeTemp = snd(temperaturaBase(deTemp))
-              minParaTemp = fst(temperaturaBase(paraTemp))
-              maxParaTemp = snd(temperaturaBase(paraTemp))
+converteEscala :: Double -> Temperatura -> Temperatura -> (Double, Temperatura)
+converteEscala graus deTemp paraTemp = ((((graus - minDeTemp) * (maxParaTemp - minParaTemp) / (maxDeTemp - minDeTemp)) + minParaTemp), paraTemp)
+    where minDeTemp = fst(temperaturaBase(deTemp))
+          maxDeTemp = snd(temperaturaBase(deTemp))
+          minParaTemp = fst(temperaturaBase(paraTemp))
+          maxParaTemp = snd(temperaturaBase(paraTemp))
 
 
     
     -- Exercicio 3 - Implement uma funçao que simule o vencedor de uma partida de pedra, papel e tesoura usando tipos craidos. Casos de empate devem ser considerados em seu tipo.
 
-    data OpcoesJogo = Pedra | Papel | Tesoura deriving (Enum, Eq, Ord)
-    data ResultadoJogo = Venceu | Perdeu | Empate deriving (Show)
+data OpcoesJogo = Pedra | Papel | Tesoura deriving (Enum, Eq, Ord)
+data ResultadoJogo = Venceu | Perdeu | Empate deriving (Show)
 
-    jogarPedraPapelTesoura :: OpcoesJogo -> OpcoesJogo -> (ResultadoJogo, ResultadoJogo)
-    jogarPedraPapelTesoura Tesoura Pedra = (Perdeu, Venceu)
-    jogarPedraPapelTesoura Pedra Tesoura = (Venceu, Perdeu)
-    jogarPedraPapelTesoura opc1 opc2 | opc1 == opc2 = (Empate, Empate)
-    jogarPedraPapelTesoura opc1 opc2 | opc1 > opc2 = (Venceu, Perdeu)
-    jogarPedraPapelTesoura opc1 opc2 | opc1 < opc2 = (Perdeu, Venceu)
+jogarPedraPapelTesoura :: OpcoesJogo -> OpcoesJogo -> (ResultadoJogo, ResultadoJogo)
+jogarPedraPapelTesoura Tesoura Pedra = (Perdeu, Venceu)
+jogarPedraPapelTesoura Pedra Tesoura = (Venceu, Perdeu)
+jogarPedraPapelTesoura opc1 opc2 | opc1 == opc2 = (Empate, Empate)
+jogarPedraPapelTesoura opc1 opc2 | opc1 > opc2 = (Venceu, Perdeu)
+jogarPedraPapelTesoura opc1 opc2 | opc1 < opc2 = (Perdeu, Venceu)
 
     -- Exercicio 4 - Faça uma função que retorne uma string com todas as vogais maiusculas e minusculas eliminadas de uma string passada por parametro usando list compreenshion
 
-    removeConsoantes :: String -> String
-    removeConsoantes palavra = [x | x <- palavra, elem x "aeiouAEIOU"]
+removeConsoantes :: String -> String
+removeConsoantes palavra = [x | x <- palavra, elem x "aeiouAEIOU"]
